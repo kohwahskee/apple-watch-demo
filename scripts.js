26e8328f9c4ea9ac54c266ae388445c825343b4f
kohwahskee <hidonbrush@gmail.com>
2022-12-24 05:37:30 -0600

    Refactored shouldScale if/elses with ternary operation

--- scripts.js
+++ scripts.js
@@ -4,16 +4,11 @@ const numOfRows = 3; // must be odd
 const minIconInRow = 1;
 const midRow = Math.round(numOfRows / 2);
 const colorChoices = ['red', 'purple', 'green', 'blue'];
-// const colorChoices = ['red'];
 
 function addNewIcon(parentNode) {
   const appIcon = document.createElement('div');
   appIcon.className = `app-icon ${colorChoices[Math.floor((Math.random() * colorChoices.length))]}`;
 
-  // // DEBUG ONLY
-  // const debugContainer = document.createElement('div');
-  // debugContainer.className = 'debug-container';
-  // debugContainer.append(appIcon);
   parentNode.append(appIcon);
 }
 for (i = minIconInRow; i <= midRow; i++) {
@@ -37,6 +32,7 @@ for (i = numOfRows; i >= midRow + minIconInRow; i--) {
 
 // Dragging Event
 const watchFace = document.getElementById('watch-face');
+const iconList = Array.from(document.getElementsByClassName('app-icon'));
 watchFace.addEventListener("mousedown", e => {
   document.addEventListener("mousemove", draggingHandler)
 })
@@ -44,7 +40,8 @@ watchFace.addEventListener("mousedown", e => {
 document.addEventListener('mouseup', e => {
   document.removeEventListener('mousemove', draggingHandler);
 })
-scaleToFit();
+
+scaleToFit(watchFace, iconList);
 
 function draggingHandler(e) {
   e.preventDefault();
@@ -52,20 +49,15 @@ function draggingHandler(e) {
   appContainer.style.top = (Number(appContainer.style.top.substring(0, appContainer.style.top.length - 2)) + e.movementY) + 'px';
   appContainer.style.left = (Number(appContainer.style.left.substring(0, appContainer.style.left.length - 2)) + e.movementX) + 'px';
 
-  // // DEVELOPMENT
-  // const debugMovementX = document.getElementById('movementX');
-  // const debugMovementY = document.getElementById('movementY');
-  // debugMovementX.innerText = e.movementX;
-  // debugMovementY.innerText = e.movementY;
-
-
-  scaleToFit();
+  scaleToFit(watchFace, iconList);
 }
 
-function scaleToFit() {
+
+
+function scaleToFit(parentContainer, iconList) {
   // COLLISION DETECTION
-  const iconList = Array.from(document.getElementsByClassName('app-icon'));
-  const parentBounds = watchFace.getBoundingClientRect();
+  const parentBounds = parentContainer.getBoundingClientRect();
+
   iconList.forEach(icon => {
     const iconBound = icon.getBoundingClientRect();
     const currentScale = parseFloat(icon.style.transform.substring(6)) || 1;
@@ -75,25 +67,6 @@ function scaleToFit() {
       x: iconBound.left + (iconBound.width / 2),
       y: iconBound.top + (iconBound.height / 2)
     }
-
-
-
-
-    // // ============== DEVELOPMENT ONLY ==============
-    // document.getElementsByClassName('left-cord')[0].innerText = `${Math.round(parentBounds.left * 10000) / 10000}px`;
-    // document.getElementsByClassName('right-cord')[0].innerText = `${Math.round(parentBounds.right * 10000) / 10000}px`;
-    // document.getElementsByClassName('top-cord')[0].innerText = `${Math.round(parentBounds.top * 10000) / 10000}px`;
-    // document.getElementsByClassName('bottom-cord')[0].innerText = `${Math.round(parentBounds.bottom * 10000) / 10000}px`;
-
-    // const iconLeftCord = document.getElementsByClassName('icon-left-cord')[0];
-    // const yCord = document.getElementsByClassName('y-cord')[0];
-    // const xCord = document.getElementsByClassName('x-cord')[0];
-    // iconLeftCord.innerText = iconBound.left;
-    // yCord.innerText = Math.round(newIconMiddlePoint.y * 1000) / 1000;
-    // xCord.innerText = Math.round(newIconMiddlePoint.x * 1000) / 1000;
-
-
-
 
     // If icon is visible inside canvas
     if (newIconMiddlePoint.x > parentBounds.left
@@ -111,40 +84,16 @@ function scaleToFit() {
         verticalScale;
 
       if (shouldLeftScale) {
-        // need scale down
-        if (iconBound.left < parentBounds.left) {
-          horizontalScale = iconScale(parentBounds.left, iconBound.left, "down");
-        }
-        else {
-          horizontalScale = iconScale(parentBounds.left, iconBound.left, "up");
-        }
+        horizontalScale = iconScale(parentBounds.left, iconBound.left, iconBound.left < parentBounds.left ? "down" : "up");
       }
       if (shouldTopScale) {
-        // need scale down
-        if (iconBound.top < parentBounds.top) {
-          verticalScale = iconScale(parentBounds.top, iconBound.top, "down");
-        }
-        else {
-          verticalScale = iconScale(parentBounds.top, iconBound.top, "up");
-        }
+        verticalScale = iconScale(parentBounds.top, iconBound.top, iconBound.top < parentBounds.top ? "down" : "up");
       }
       if (shouldRightScale) {
-        // need scale down
-        if (iconBound.right > parentBounds.right) {
-          horizontalScale = iconScale(parentBounds.right, iconBound.right, "down");
-        }
-        else {
-          horizontalScale = iconScale(parentBounds.right, iconBound.right, "up");
-        }
+        horizontalScale = iconScale(parentBounds.right, iconBound.right, iconBound.right > parentBounds.right ? "down" : "up");
       }
       if (shouldBottomScale) {
-        // need scale down
-        if (iconBound.bottom > parentBounds.bottom) {
-          verticalScale = iconScale(parentBounds.bottom, iconBound.bottom, "down");
-        }
-        else {
-          verticalScale = iconScale(parentBounds.bottom, iconBound.bottom, "up");
-        }
+        verticalScale = iconScale(parentBounds.bottom, iconBound.bottom, iconBound.bottom > parentBounds.bottom ? "down" : "up");
       }
 
       const newBoundAfterHorizontalScale = {
@@ -177,8 +126,6 @@ function scaleToFit() {
         newScale = horizontalScale || verticalScale;
       }
 
-
-
       // If all 4 shouldScale are false -> no scale (Basically when icon is in the middle of canvas and isn't close to any edge, then automatically scale to full size)
       // This prevents iconScale() from behaving weirdly when it's supposed to calculate scale when icon is at full scale (1)
       // Mouse Event sucks.
@@ -191,7 +138,6 @@ function scaleToFit() {
       icon.style.transform = `scale(0.01)`;
     }
 
-
     function iconScale(baseParentBound, baseIconBound, scaleDirection) {
       let newScale, distance;
       distance = Math.abs(baseParentBound - baseIconBound);
@@ -203,7 +149,7 @@ function scaleToFit() {
       }
       if (newScale > 1) { newScale = 1 }
       else if (newScale <= 0.001) { newScale = 0.001 };
-      // icon.style.transform = `scale(${newScale})`;
+
       return newScale;
     }
   })
